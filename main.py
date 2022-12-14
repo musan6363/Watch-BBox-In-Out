abf318a542b38340e27eedbdd3a95dfe6c8f71e2
musan6363 <msn151336+prog@gmail.com>
2022-12-14 03:02:58 +0000

    update: アノテーション点が画像内か否か，オブジェクト内か否か，何を見ているかを探索するプログラム

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import argparse
 from glob import glob
+from tqdm import tqdm
 import uuid
 import json
 import ndjson
 from os import path as osp
+import statistics
 
 FRAME_SIZE = (1980, 1200)  # nuImages
 N_ANNOTATE = 3
-ANN_DATA_PATH = "/home0/murakamih/annotation/v221130/nuimages_ped_1017/v1.0-train/json"
 
 class GazeTargetObject:
     def __init__(self, token: str, bbox: list, category: str) -> None:
@@ -29,9 +30,27 @@ class GazePoint:
         self.loc = loc
         self.is_difficult = is_difficult
         self.is_eyecontact = is_eyecontact
-        self.is_inside_frame = True if 0 < self.loc[0] < FRAME_SIZE[0] and 0 < self.loc[1] < FRAME_SIZE[1] else False
+        self.is_inside_frame = self.check_inside_frame()
         self.is_inside_object = False
         self.gto = []
+
+    def check_inside_frame(self) -> bool:
+        if self.is_eyecontact:
+            return False
+        if 0 < self.loc[0] < FRAME_SIZE[0] and 0 < self.loc[1] < FRAME_SIZE[1]:
+            return True
+        return False
+
+    def add_gto(self, obj: GazeTargetObject) -> bool:
+        if self.is_eyecontact:
+            return False
+        if obj.token == self.ped_token:
+            return False
+        if obj.is_inside(self.loc):
+            self.gto.append(obj)
+            self.is_inside_object = True
+            return True
+        return False
 
 class Pedestrian:
     def __init__(self, token: str, frame_token: str, bbox: list) -> None:
@@ -41,18 +60,46 @@ class Pedestrian:
         self.gaze = []
         self.gto = []
 
-    def set_gaze_point(self, look: list, difficult: list, eyecontact: list) -> list:
+    def set_gaze_point(self, look: list, difficult: list, eyecontact: list) -> None:
         for i in N_ANNOTATE:
             gp = GazePoint(self.token, self.frame_token, tuple(look[i]), difficult[i], eyecontact[i])
             self.gaze.append(gp)
-        return self.gaze
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Count what look the gaze point')
-    parser.add_argument('ann_json', help='ex) /home0/murakamih/annotation/v221205/nuimages_ped_1017/v1.0-train/json')
-    parser.add_argument('save_dir', help='ex) ./output')
-    _args = parser.parse_args()
-    return _args
+    def add_gto(self, gto: GazeTargetObject) -> None:
+        '''
+        現状はGazePoint内のgtoと変わらない
+        将来的に，difficultフラグや多数決を経て，そのpedが見ている代表オブジェクトを決定した上で格納したい
+        '''
+        self.gto.append(gto)
+
+class Frame:
+    def __init__(self, token: str) -> None:
+        self.token = token
+        self.peds = []
+        self.objects = []
+
+        # 統計情報
+        self.looking_eyecontact = 0
+        self.looking_inside_frame = 0
+        self.looking_inside_obj = 0
+    
+    def add_ped(self, ped: Pedestrian) -> None:
+        self.peds.append(ped)
+
+    def add_obj(self, obj: GazeTargetObject) -> None:
+        self.objects.append(obj)
+
+    def stat(self) -> None:
+        ped: Pedestrian
+        for ped in self.peds:
+            gaze: GazePoint
+            for gaze in ped.gaze:
+                if gaze.is_eyecontact:
+                    self.looking_eyecontact += 1
+                if gaze.is_inside_frame:
+                    self.looking_inside_frame += 1
+                if gaze.is_inside_object:
+                    self.looking_inside_obj += 1
 
 def read_ndjson(path: str) -> list:
     with open(path, 'r') as f:
@@ -61,18 +108,79 @@ def read_ndjson(path: str) -> list:
         _data = json.loads(_tmp)
     return _data
 
+def read_json(path: str) -> list:
+    with open(path, 'r') as f:
+        _data = json.load(f)
+    return _data
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ped_ann', help='ex) ~/annotation/v221130/nuimages_ped_1017/v1.0-mini/json', type=str)
+    parser.add_argument('obj_dataset', help='ex) ~/work/Watch-BBox-In-Out/object-extract/output/nuImages/v1.0-mini', type=str)
+    args = parser.parse_args()
+    return args
+
+def check_looking(peds: list, obj: GazeTargetObject) -> None:
+    ped: Pedestrian
+    for ped in peds:
+        gp: GazePoint
+        for gp in ped.gaze:
+            is_looking = gp.add_gto(obj)  # 各アノテーション点がobjを見ているか調査．見ていればリストに追加してTrueを返す．
+            if is_looking:
+                ped.add_gto(obj)
+
+
 def main():
     args = parse_args()
-    ann_jsons = glob(args.ann_json+"/*.json")
-    dataset_name = args.ann_json.split('/')[-3]
-    version_name = args.ann_json.split('/')[-2]
-    for ann_json in ann_jsons:
-        image_token = osp.splitext(osp.basename(ann_json))[0]
-        print(dataset_name, version_name, image_token)
-        records = read_ndjson(ann_json)
-        for record in records:
-            print(record)
-        break
+    
+    frames = {}
+    for ped_record in tqdm(glob(args.ped_ann + '/*.json'), "running record..."):
+        frame_id = osp.splitext(osp.basename(ped_record))[0]
+        frame = Frame(frame_id)
+
+        # 歩行者の登録
+        ped_annotations: list = read_ndjson(ped_record)
+        ped_annotation: dict
+        for ped_annotation in ped_annotations:
+            ped = Pedestrian(ped_annotation['token'], frame_id, ped_annotation['bbox'])
+            ped.set_gaze_point(ped_annotation['look'], ped_annotation['difficult'], ped_annotation['eyecontact'])
+            frame.add_ped(ped)
+
+        # オブジェクトの登録
+        dataset_objects: dict = read_json(args.obj_dataset + '/' + frame_id + '.json')
+        object_token: str
+        dataset_object: dict
+        for object_token, dataset_object in dataset_objects.items():
+            obj = GazeTargetObject(object_token, dataset_object['bbox'], dataset_object['category'])
+            frame.add_obj(obj)
+            check_looking(frame.peds, obj)  # オブジェクトを各視点が見ているか確認し，格納
+
+        # frameごとの統計情報
+        frame.stat()
+        frames[frame_id] = frame
+
+    # 統計情報の表示
+    cnt_peds = []
+    cnt_objs = []
+    cnt_looking_eyecontact = []
+    cnt_looking_inside_frame = []
+    cnt_looking_inside_obj = []
+    frame: Frame
+    for frame in tqdm(frames.values(), "statistics..."):
+        cnt_peds.append(len(frame.peds))
+        cnt_objs.append(len(frame.objects))
+        cnt_looking_eyecontact.append(frame.looking_eyecontact)
+        cnt_looking_inside_frame.append(frame.looking_inside_frame)
+        cnt_looking_inside_obj.append(frame.looking_inside_obj)
+    print(f"歩行者総数 -> {sum(cnt_peds)}")
+    print(f"フレームあたりの歩行者数 -> {statistics.mean(cnt_peds)}±{statistics.stdev(cnt_peds)}")
+    print(f"オブジェクト総数 -> {sum(cnt_objs)}")
+    print(f"フレームあたりのオブジェクト数 -> {statistics.mean(cnt_objs)}±{statistics.stdev(cnt_objs)}")
+    cnt_gaze_points = sum(cnt_peds)*3
+    print(f"総アノテーション点数 -> {cnt_gaze_points}")
+    print(f"アイコンタクト点数 -> {cnt_looking_eyecontact}({cnt_looking_eyecontact*100/cnt_gaze_points}%)")
+    print(f"フレーム内を見ている点数 -> {cnt_looking_inside_frame}({cnt_looking_inside_frame*100/cnt_gaze_points}%)")
+    print(f"オブジェクト内を見ている点数 -> {cnt_looking_inside_obj}({cnt_looking_inside_obj*100/cnt_gaze_points}%)")
 
 if __name__ == "__main__":
     main()
